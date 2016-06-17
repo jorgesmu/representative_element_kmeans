118382ffbcd56494c1b3edb58934b3e480a7b08a
Jorge Smulevici <jorge@MacBook-Pro-de-Jorge.local>
2016-06-17 15:04:56 -0300

    Clusters by domain

--- lib/representative_element_kmeans/kmeans.rb
+++ lib/representative_element_kmeans/kmeans.rb
@@ -1,10 +1,50 @@
+require 'k_means'
+
 module RepresentativeElementKmeans
   class Kmeans
   	attr_reader :keys, :elements
 
-  	def initialize(elements_map)
+
+  	def initialize(elements_map, opts={})
   		@keys = elements_map.keys
   		@elements = elements_map.values
+  		@opts = opts
+  		clusterize 
+  	end
+
+  	def centroids
+  		@kmeans_manager.centroids
+  	end
+
+  	def clusters
+  		clusters_by_domain @keys
+  	end
+  	alias_method :clusters_by_ids, :clusters
+
+  	def clusters_by_index
+  		@kmeans_manager.view
+  	end
+
+  	def clusters_by_elements
+  		clusters_by_domain @elements
+  	end
+
+  	private
+
+  	def clusters_by_domain domain_elements
+	  	clusters_by_index.map do |cluster|
+	  		cluster.map do |element_index|
+	  			domain_elements[element_index]
+	  		end
+	  	end
+    end
+
+  	def clusterize
+			cluster_manager
+  	end
+
+  	def cluster_manager
+  		@kmeans_manager ||= KMeans.new(@elements, @opts)
   	end
   end
 end
--- spec/kmeans_spec.rb
+++ spec/kmeans_spec.rb
@@ -1,18 +1,26 @@
 require 'spec_helper'
 
 describe RepresentativeElementKmeans::Kmeans do
-  let(:elements) { {"1" => [1], "2" => [2], "3" =>[3]} }
+  let(:elements) { {"1" => [1,1,1], "2" => [1,2,3], "3" =>[3,2,10], "4" => [0,5,2]} }
   let(:kmeans) { RepresentativeElementKmeans::Kmeans.new elements }
 
   it 'not nil' do
-    expect(kmeans).not_to eq(nil)
+    expect(kmeans).not_to eq nil 
   end
 
   it 'keys' do
-    expect(kmeans.keys).to eq(elements.keys)
+    expect(kmeans.keys).to eq ["1", "2", "3", "4"]
   end
 
-  it 'elements' do
-    expect(kmeans.elements).to eq(elements.values)
+  it 'cluster_by_ids' do
+  	kmeans.stub(:clusters_by_index).and_return([[0,2],[1],[3]])
+
+  	expect(kmeans.clusters_by_ids).to eq [["1","3"],["2"],["4"]]
+  	expect(kmeans.clusters_by_ids).to eq kmeans.clusters_by_ids
+  end
+
+  it 'clusters_by_elements' do
+  	kmeans.stub(:clusters_by_index).and_return([[0,2],[1],[3]])
+  	expect(kmeans.clusters_by_elements).to eq [[elements["1"],elements["3"]],[elements["2"]], [elements["4"]]]
   end
 end
